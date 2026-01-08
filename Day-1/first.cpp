b58cfb2dd64025f06c0a15e07eccb03854436d85
Yash Saini <yashsaini10@outlook.in>
2026-01-08 22:53:21 +0530

    first.cpp

---
+++ Day-1/first.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    float b = 2.41;
+    char c = 'a';
+    long long d = 327298314793712;
+    bool num = false;
+    string name = "Rohit";
+
+    cout<<"Hello Coder Army"<<endl;
+
+    cout<<a<<endl;
+    cout<<sizeof(a)<<endl;
+
+    cout<<name<<endl;
+    cout<<name.length();
+
+    return 0;
+}

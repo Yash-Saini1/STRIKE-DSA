f9d5f3247b7891d0824c93d4123f73dc72817015
Yash Saini <yashsaini10@outlook.in>
2026-01-09 23:26:13 +0530

    first.cpp

---
+++ Day-2/first.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout<<"Enter Your First Number: ";
+    cin>>a;
+    cout<<"Enter Your Second Number: ";
+    cin>>b;
+
+    cout<<"Your sum is: "<<a+b<<endl;
+
+    char c = 'a'+1;
+    cout<<c<<endl;
+
+    int d = 'a'+1;
+    cout<<d;
+
+    return 0;
+}
